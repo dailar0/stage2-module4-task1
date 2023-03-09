322d18a2c8d5fe5ce805e76caaf0b954b10e4d3e
dailar <dailarse00@gmail.com>
2023-03-09 19:44:44 +0300

    done?

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,25 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    @Override
+    public Connection createConnection() {
+        try (InputStream stream = this.getClass().getClassLoader().getResourceAsStream("h2database.properties")) {
+            Properties properties = new Properties();
+            properties.load(stream);
+
+            String url = properties.getProperty("db_url");
+            return DriverManager.getConnection(url, properties);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
 
